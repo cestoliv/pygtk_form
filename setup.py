63140e1f01cc4d5b8c5ffbd185837561d67473d8
Olivier Cartier <cestoliv@chevro.fr>
2020-09-21 09:49:07 +0200

    adding __init__.py in fields and MANIFEST.in to be sure that every files is included

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygtk-form",
-    version="0.0.1",
+    version="0.0.2",
     author="Olivier Cartier",
     author_email="cestoliv@chevro.fr",
     description="Create forms very easily for your scripts with GTK !",
